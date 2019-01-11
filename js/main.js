60357089d97d13b2c4df30cb844b6979121796b0
Vitaliy Bobyk <falcone159357bv@gmail.com>
2019-01-11 22:43:46 +0200

    inicialized new repository

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      // items: 3,
+      margin: 10,
+      smartSpeed: 1000,
+      loop: true,
+      nav: true,
+    });
+  });
